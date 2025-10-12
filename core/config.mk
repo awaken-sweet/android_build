4bee9a51540dfe646051994de05a70b17e807169
hridaya <hridaya@pixelos.net>
2025-10-12 09:28:13 +0545

    config: Include our custom sepolicy Makefile
    Change-Id: Id3a3b4730327d06d0709b23e9bca82bdd976e506

--- core/config.mk
+++ core/config.mk
@@ -1294,6 +1294,7 @@ ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/lineage/sepolicy/common/sepolicy.mk)
+$(eval include device/custom/sepolicy/sepolicy.mk)
 endif
 endif
 
